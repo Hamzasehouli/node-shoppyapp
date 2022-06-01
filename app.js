1701c1814a817c6124abf8f621b8c21302ad6f23
hamzasehouli <sehoulihamza@yahoo.com>
2022-06-01 13:35:37 +0100

    Added cookieParser in app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   console.log("get /");
